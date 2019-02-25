eb31c3e72eb26c8373741fa18227267ab02e947a
Dave Gaeddert <dave.gaeddert@gmail.com>
2019-02-25 14:33:25 -0600

    Don't add 'version' field

--- src/collect.py
+++ src/collect.py
@@ -50,9 +50,6 @@ def collect():
         try:
             response = requests.get(f'https://api.wordpress.org/plugins/info/1.0/{plugin}.json')
             available = list(response.json().get('versions', {}).keys())
-            version = response.json()['version']
-            if version not in available:
-                available.append(version)
             print(available)
         except Exception:
             logging.error(f'Unable to find available versions of {plugin} in API.')
